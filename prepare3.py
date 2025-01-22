ae3c2f77ac760e16829bad4e1dc4e3fb70986b35
braydenbarger <bar21065@byui.edu>
2025-01-22 10:40:13 -0700

    Initial commit

---
+++ prepare3.py
@@ -0,0 +1,12 @@
+age = int(input("How old are you? "))
+next_birthday_age = age + 1
+print(f"On your next birthday you will be {next_birthday_age}")
+
+egg_cartons = int(input("How many egg cartons do you have? "))
+total_eggs = egg_cartons * 12
+print(f"You have {total_eggs} eggs")
+
+cookies = int(input("How many cookies do you have? "))
+people = int(input("How many people are there? "))
+cookies_per_person = cookies / people
+print(f"Each person may have {cookies_per_person} cookies")
---
+++ prove.py
@@ -0,0 +1,5 @@
+favorite_color = input("Please type your favorite color: ")
+
+reason = input("Why do you like " + favorite_color + "?")
+
+print(favorite_color + " is a great favorite color, because " + reason + ".")
