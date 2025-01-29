ebe244353d4e6d3029cc1b3a8e3255d2f98a643c
braydenbarger <bar21065@byui.edu>
2025-01-29 09:42:06 -0700

    meal calculator

--- prove3.py
+++ prove3.py
@@ -1,3 +1,10 @@
+from datetime import date
+from datetime import datetime
+
+today = date.today()
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+
 child_price = float(input("What is the price of a child's meal? "))
 adult_price = float(input("What is the price of an adult's meal? "))
 child_number = int(input("How many children are there? "))
@@ -5,7 +12,9 @@ adult_number = int(input("How many adults are there? "))
 tax_rate = float(input("What is the sales tax rate? "))
 tip = float(input("What is the tip amount? "))
 
-subtotal = (child_price * child_number) + (adult_price * adult_number) + tip
+child_total = (child_price * child_number)
+adult_total = (adult_price * adult_number)
+subtotal = child_total + adult_total + tip
 tax = subtotal * (tax_rate / 100)
 total = subtotal + tax
 
@@ -16,3 +25,19 @@ print(f"Total: ${total:.2f}")
 payment = float(input("What is the payment amount? "))
 change = payment - total
 print(f"Change: ${change:.2f}")
+
+reciept = (f'''
+            ----------------------------------------
+            Date: {today}
+            Time: {current_time}
+            QTY of Child Meals: {child_number} Child Meals Price: {child_total:.2f}
+            QTY of Adult Meals: {adult_number} Adult Meals Price: {adult_total:.2f}
+            Subtotal: {subtotal:.2f}
+            Tax: {tax:.2f}
+            Tip: {tip:.2f}
+            Total: {total:.2f}
+            Payment: {payment:.2f}
+            Change: {change:.2f}
+''')
+
+print(reciept)
