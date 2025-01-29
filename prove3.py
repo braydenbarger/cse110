04ee41999e0caef1ec10a9a027c5ebf54ca3f2f1
braydenbarger <bar21065@byui.edu>
2025-01-29 09:27:38 -0700

    meal calc

--- prove3.py
+++ prove3.py
@@ -3,11 +3,16 @@ adult_price = float(input("What is the price of an adult's meal? "))
 child_number = int(input("How many children are there? "))
 adult_number = int(input("How many adults are there? "))
 tax_rate = float(input("What is the sales tax rate? "))
+tip = float(input("What is the tip amount? "))
 
-subtotal = (child_price * child_number) + (adult_price * adult_number)
+subtotal = (child_price * child_number) + (adult_price * adult_number) + tip
 tax = subtotal * (tax_rate / 100)
 total = subtotal + tax
 
 print(f"Subtotal: ${subtotal:.2f}")
 print(f"Sales Tax: ${tax:.2f}")
 print(f"Total: ${total:.2f}")
+
+payment = float(input("What is the payment amount? "))
+change = payment - total
+print(f"Change: ${change:.2f}")
